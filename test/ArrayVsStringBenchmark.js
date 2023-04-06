656264faf28f39c97583283fac59df6efefe55d0
Mathieu Schalkwijk <mathieu.schalkwijk@gmail.com>
2023-04-06 19:05:07 +0200

    Update print for test

--- test/ArrayVsStringBenchmark.js
+++ test/ArrayVsStringBenchmark.js
@@ -4,11 +4,11 @@ const { findBestMoveStringToArray } = require('../src/evalFunc.js');
 
 
 let t1 = performance.now()
-console.log(findBestMoveString("m00000h00000mm0000hmh000h00000h00000000000", "m"))
+console.log("Calling method with computation on the string: "+findBestMoveString("m00000h00000mm0000hmh000h00000h00000000000", "m"))
 let t2 = performance.now()
 console.log(`Call to string findBestMove took ${t2 - t1} milliseconds.`)
 
 t1 = performance.now()
-console.log(findBestMoveStringToArray("m00000h00000mm0000hmh000h00000h00000000000", "m"))
+console.log("Calling method with translation into an 2D array: "+findBestMoveStringToArray("m00000h00000mm0000hmh000h00000h00000000000", "m"))
 t2 = performance.now()
 console.log(`Call to stringToArray findBestMove took ${t2 - t1} milliseconds.`)
