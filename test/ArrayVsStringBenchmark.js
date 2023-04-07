de3a82d6fc852b2bd2373c35e10d01150e4b42b5
Mathieu Schalkwijk <mathieu.schalkwijk@gmail.com>
2023-04-07 09:59:36 +0200

    Test 1000 eval

--- test/ArrayVsStringBenchmark.js
+++ test/ArrayVsStringBenchmark.js
@@ -1,14 +1,20 @@
 const { performance } = require('perf_hooks');
 const { findBestMoveString } = require('../src/evalFuncString.js');
-const { findBestMoveStringToArray } = require('../src/evalFunc.js');
+const { findBestMoveStringToArray } = require('../src/evalFuncArray.js');
 
 
 let t1 = performance.now()
-console.log("Calling method with computation on the string: "+findBestMoveString("m00000h00000mm0000hmh000h00000h00000000000", "m"))
+console.log("Calling 1000 times the method with computation on the string: "+findBestMoveString("m00000h00000mm0000hmh000h00000h00000000000", "m"))
+for (let i = 0; i < 1000; i++) {
+    findBestMoveString("m00000h00000mm0000hmh000h00000h00000000000", "m")
+}
 let t2 = performance.now()
-console.log(`Call to string findBestMove took ${t2 - t1} milliseconds.`)
+console.log(`Call to 1000 findBestMoveString took ${t2 - t1} milliseconds.`)
 
 t1 = performance.now()
-console.log("Calling method with translation into an 2D array: "+findBestMoveStringToArray("m00000h00000mm0000hmh000h00000h00000000000", "m"))
+console.log("Calling 1000 times th method with translation into an 2D array: ")
+for (let i = 0; i < 1000; i++) {
+    findBestMoveStringToArray("m00000h00000mm0000hmh000h00000h00000000000", "m")
+}
 t2 = performance.now()
-console.log(`Call to stringToArray findBestMove took ${t2 - t1} milliseconds.`)
+console.log(`Call to 1000 findBestMoveStringToArray took ${t2 - t1} milliseconds.`)
