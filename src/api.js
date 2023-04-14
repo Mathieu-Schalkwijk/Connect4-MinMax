1794b3e7e966174599e99750bd3316965e711c16
Mathieu Schalkwijk <mathieu.schalkwijk@gmail.com>
2023-04-14 11:11:29 +0200

    Serve game.html from api.js
    On connection to localhost:3000

--- src/api.js
+++ src/api.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const { findBestMoveStringToArray } = require('./evalFuncArray');
+
+// Middleware for serving static files from the "front" folder
+app.use(express.static(path.join(__dirname, 'front')));
 
 // Route for receiving GET requests
 app.get('/move', (req, res) => {
@@ -19,6 +23,11 @@ app.get('/move', (req, res) => {
     if (columnToPlay === -1) return res.status(420).json({ column: "gameover" });
 
     res.status(200).json({ column: columnToPlay });
+});
+
+// Route for serving game.html on root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front', 'game.html'));
 });
 
 app.listen(PORT, () => {
